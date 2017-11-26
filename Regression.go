c39dc317e047110e08debd117320eb268d9c945c
knlmathur <knlmathur@gmail.com>
2017-11-26 18:58:24 +0100

    3rd Commit

--- Regression.go
+++ Regression.go
@@ -1,8 +1,12 @@
 package Regression
 
-import (
-	"gonum.org/v1/gonum/stat"
-)
+import "gonum.org/v1/gonum/stat"
+
+// TValue Calculates the T-Stat of the intercept
+func TValue(beta float64, x []float64) float64 {
+	tval := beta / stat.StdErr(stat.StdDev(x, nil), float64(len(x)))
+	return tval
+}
 
 //LinearRegression performs the actual Regression
 //Outputs the results into the respective channels
--- RegressionTest.go
+++ RegressionTest.go
@@ -3,8 +3,6 @@ package Regression
 import (
 	"fmt"
 	"math/rand"
-
-	"gonum.org/v1/gonum/stat"
 )
 
 //NumberGenerator creates an examle using in built random number library
@@ -19,36 +17,6 @@ func NumberGenerator(iter int) (x []float64, y []float64) {
 		y[i] = rand.NormFloat64()
 	}
 	return x, y
-}
-
-// TValue Calculates the T-Stat of the intercept
-func TValue(beta float64, x []float64) float64 {
-	tval := beta / stat.StdErr(stat.StdDev(x, nil), float64(len(x)))
-	return tval
-}
-
-//LinearRegression performs the actual Regression
-//Outputs the results into the respective channels
-func LinearRegression(iter int, coef chan float64, intercept chan float64, rsq chan float64, tval chan float64) {
-
-	// Generate Random Numbers
-	x, y := NumberGenerator(iter)
-
-	//Regress
-	alpha, beta := stat.LinearRegression(x, y, nil, false)
-
-	//RSquared
-	rsquared := stat.RSquared(x, y, nil, alpha, beta)
-
-	//Find T Value
-	tvalue := TValue(beta, x)
-
-	// Assign to Channels
-	coef <- alpha
-	intercept <- beta
-	rsq <- rsquared
-	tval <- tvalue
-
 }
 
 func main() {
